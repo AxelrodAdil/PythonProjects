a83f2b9d8bbe0da8d6846dc9ac884af2c77ebc8c
AxelrodAdil <taraza309@gmail.com>
2022-07-05 12:12:18 +0600

    [AM-P2] - DeepFace methods impl

---
+++ JustForFun/DeepFaceImpl.py
@@ -0,0 +1,47 @@
+from deepface import DeepFace
+import json
+
+
+def face_verify(img_1, img_2):
+    try:
+        result = DeepFace.verify(img1_path=img_1, img2_path=img_2)
+
+        with open('resultDeepFace.json', 'w') as file:
+            json.dump(result, file, indent=4, ensure_ascii=False)
+        # return result
+
+        if result.get('verified'):
+            return 'YES'
+        return 'NO'
+    except Exception as e:
+        return e
+
+
+def face_recognize():
+    try:
+        result = DeepFace.find(img_path='resources/hl1.jpg', db_path='DeepFaceDB')
+        result = result.values.tolist()
+        return result
+    except Exception as e:
+        return e
+
+
+def face_analyze():
+    try:
+        result = DeepFace.analyze(img_path='resources/hl2.jpg', actions=['age', 'gender', 'race', 'emotion'])
+        # result = DeepFace.analyze(img_path='DeepFaceDB/as1.jpg', actions=['age', 'gender', 'race', 'emotion'])
+        with open('result_analyze_DeepFace.json', 'w') as file:
+            json.dump(result, file, indent=4, ensure_ascii=False)
+        return result
+    except Exception as e:
+        return e
+
+
+def main():
+    # print(face_verify(img_1='resources/hl1.jpg', img_2='resources/hl2.jpg'))
+    # print(face_recognize())
+    print(face_analyze())
+
+
+if __name__ == '__main__':
+    main()
