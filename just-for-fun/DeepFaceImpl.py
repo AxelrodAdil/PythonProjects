69452f5c059ebce08b17254cf255f739484c9b17
AxelrodAdil <taraza309@gmail.com>
2022-07-05 13:59:29 +0600

    [AM-P2] - DeepFace - 'CC'

--- just-for-fun/DeepFaceImpl.py
+++ just-for-fun/DeepFaceImpl.py
@@ -6,7 +6,7 @@ def face_verify(img_1, img_2):
     try:
         result = DeepFace.verify(img1_path=img_1, img2_path=img_2)
 
-        with open('resultDeepFace.json', 'w') as file:
+        with open('result_DeepFace.json', 'w') as file:
             json.dump(result, file, indent=4, ensure_ascii=False)
         # return result
 
@@ -19,7 +19,7 @@ def face_verify(img_1, img_2):
 
 def face_recognize():
     try:
-        result = DeepFace.find(img_path='resources/hl1.jpg', db_path='DeepFaceDB')
+        result = DeepFace.find(img_path='resources/hl1.jpg', db_path='deepFaceDB')
         result = result.values.tolist()
         return result
     except Exception as e:
@@ -29,7 +29,7 @@ def face_recognize():
 def face_analyze():
     try:
         result = DeepFace.analyze(img_path='resources/hl2.jpg', actions=['age', 'gender', 'race', 'emotion'])
-        # result = DeepFace.analyze(img_path='DeepFaceDB/as1.jpg', actions=['age', 'gender', 'race', 'emotion'])
+        # result = DeepFace.analyze(img_path='deepFaceDB/as1.jpg', actions=['age', 'gender', 'race', 'emotion'])
         with open('result_analyze_DeepFace.json', 'w') as file:
             json.dump(result, file, indent=4, ensure_ascii=False)
         return result
